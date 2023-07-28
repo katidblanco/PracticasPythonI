8d1a562e182685d99c2c603116677cad88f3c2e0
katidblanco <katidblanco@gmail.com>
2023-07-27 22:23:54 -0300

    Creando ventana

---
+++ RegistrodeAlumnos/registrodeAlumnos.py
@@ -0,0 +1,29 @@
+from tkinter import *
+raiz=Tk()
+
+raiz.resizable(0,0)
+raiz.iconbitmap(r"D:\python\practicas\RegistrodeAlumnos\user-group.ico")
+#raiz.geometry("350x350")
+#raiz.config(bg="#f4f0e5")
+
+miFrame = Frame()
+miFrame.pack()
+miFrame.config(bg="#c3e6d4")
+miFrame.config(width="350",height="350")
+miFrame.config(relief="solid")
+miFrame.config(bd=5)
+miFrame.config(cursor="hand2")
+
+
+
+
+
+
+
+
+
+
+raiz.mainloop()
+
+
+
---
+++ RegistrodeAlumnos/registrodeAlumnos.pyw
@@ -0,0 +1,18 @@
+from tkinter import *
+raiz=Tk()
+
+raiz.resizable(0,0)
+raiz.iconbitmap(r"D:\python\practicas\RegistrodeAlumnos\user-group.ico")
+
+
+
+
+
+
+
+
+
+raiz.mainloop()
+
+
+
